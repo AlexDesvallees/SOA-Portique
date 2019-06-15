c7af2deff50c3db339f50cb97ad149081f10f3d4
Alexandre Desvallées <adesvallees@alexandres-mbp.home>
2019-06-15 11:59:19 +0200

    ajouts pour connecteur SQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+//#region Modules
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,12 +10,19 @@ import { ProgrammeModule } from "./programme/programme.module";
 import { OperateurModule } from "./operateur/operateur.module";
 import { FrequentationModule } from "./frequentation/frequentation.module";
 import { CompteModule } from "./compte/compte.module";
+//#endregion
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth-role.guard';
 
+//Connexion MySQL
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection, EntityManager } from 'typeorm'; //Voir si EntityManager est utile sinon le virer
+
 @Module({
-  imports: [PersonneModule, PortiqueModule, PanneModule, StatusModule, ProgrammeModule, OperateurModule, FrequentationModule, CompteModule],
+  imports: [PersonneModule, PortiqueModule, PanneModule, StatusModule, ProgrammeModule, OperateurModule, FrequentationModule, CompteModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: RolesGuard }],
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private readonly connection: Connection, private readonly entityManager: EntityManager) {}
+}
--- src/users/user.ts
+++ src/users/user.ts
@@ -1,13 +1,35 @@
 import { ApiModelProperty } from '@nestjs/swagger';
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
+import { CompteDTO } from 'dist/compte/compte';
+import { StatusDTO } from 'dist/status/status';
 
-// @ApiModel({description: 'test'})
-export class PersonneDTO {
+/**
+ * Documentation sur : https://github.com/typeorm/typeorm , https://docs.nestjs.com/techniques/database , https://github.com/typeorm/typeorm/blob/master/docs/multiple-connections.md , 
+ */
+
+@Entity()
+export class PersonneDTO extends BaseEntity {
   @ApiModelProperty()
+  @PrimaryGeneratedColumn()
   readonly personne_id: number;
 
   @ApiModelProperty()
+  @Column({ length: 50 })
+  readonly status_id: StatusDTO["status_id"];
+
+  @ApiModelProperty()
+  @Column({ length: 50 })
+  readonly compte_id: CompteDTO["compte_id"];
+
+  @ApiModelProperty()
+  @Column({ length: 50 })
   readonly nom: string;
 
   @ApiModelProperty()
+  @Column({ length: 50 })
   readonly prenom: string;
+
+  @ApiModelProperty()
+  @Column({ length: 1 })
+  readonly isBlocked: number; // Value in db = 0 || 1 pour false || true
 }
