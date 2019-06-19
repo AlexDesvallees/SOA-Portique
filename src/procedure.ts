a3c51d7bd95dbf4db71b334b38278ab62413a32d
Alexandre Desvallées <adesvallees@alexandres-mbp.home>
2019-06-19 23:01:17 +0200

    ajouts fichiers pour procedures stockees

--- src/procedure.ts
+++ src/procedure.ts
@@ -3,12 +3,20 @@ import { procedures } from "./procedures.json";
 
 export class Procedure{
 
+    /**
+     * Function that associate a specific name and a value from the procedure.json file
+     * @param procedures JSON file
+     */
     private GetProcedure(procedures) {
         if(procedures.name === this[0]) {
             return procedures;
         }
     }
 
+    /**
+     * Return the SQL request from a specific name
+     * @param name Name of the request to Get (or PROC_STOCK) in procedures.json file
+     */
     public GetRequest(name: string) : string {
         var myVar = procedures.find(this.GetProcedure, [name]);
         return myVar.request;
