63089d80fd6cc6f10a21bd218a402bd340503bb2
Alexandre Desvallées <adesvallees@alexandres-mbp.home>
2019-06-20 09:36:20 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { UserModule } from './users/users.module';
+import { Transport } from '@nestjs/common/enums/transport.enum';
 // import { RolesGuard } from './auth-role.guard';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  //Guard
-  // app.useGlobalGuards(new RolesGuard());
+  const appPromise = NestFactory.create(AppModule);
+  const miscroSPromise = NestFactory.createMicroservice(UserModule, {
+    transport: Transport.TCP
+  });
 
-  //Swagger
-  const options = new DocumentBuilder()
-  .setTitle('API SOA-Portiques')
-  .setDescription('The API for the SOA-Portiques Project !!')
-  .setVersion('0.2')
-  .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  Promise.all([appPromise,miscroSPromise]).then(([app,miscroS])=>{
 
-  // await app.listen(process.env.port);
-  const port = 3000;
-  console.log("Server listening on port " + port);
-  await app.listen(port);
+    //Guard
+    // app.useGlobalGuards(new RolesGuard());
+  
+    //Swagger
+    const options = new DocumentBuilder()
+    .setTitle('API SOA-Portiques')
+    .setDescription('The API for the SOA-Portiques Project !!')
+    .setVersion('0.2')
+    .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  
+    // await app.listen(process.env.port);
+    const port = 3000;
+    console.log("Server listening on port " + port);
+
+    const microSListenPromise = new Promise(resolve=>{miscroS.listen(() => {
+      console.log("MicroService OK !!");
+      resolve();
+    })});
+    return Promise.all([app.listen(port),microSListenPromise]);
+  })
 }
 bootstrap();
